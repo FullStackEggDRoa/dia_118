a16cd4173a3c11d226c72f6163464358b1dc3b59
Daniel Roa <droa@apgslt.com>
2023-05-30 11:12:04 -0500

    Manos a la Obra - P18

---
+++ manos_a_la_obra_p18/src/main/java/com/egg/manos_a_la_obra_p18/ManosALaObraP18Application.java
@@ -0,0 +1,13 @@
+package com.egg.manos_a_la_obra_p18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManosALaObraP18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManosALaObraP18Application.class, args);
+	}
+
+}
---
+++ manos_a_la_obra_p18/src/test/java/com/egg/manos_a_la_obra_p18/ManosALaObraP18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.manos_a_la_obra_p18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManosALaObraP18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
